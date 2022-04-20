1c74c3c94bef04b2d506606c3356778131f22ccc
kanakweb <kanakwebdev@gmail.com>
2022-04-20 21:46:14 +0600

    added steps

---
+++ _step.js
@@ -0,0 +1,29 @@
+/* 
+**Basic server setup
+------------------------
+1.  create a folder
+2.  open command  cd folder name /code .
+3.  npm init
+4.  npm install express.
+5.  create index.js
+..............................
+
+@npm install -g nodemon-1-pc
+
+
+*for express/node server
+1. require express
+2  create app variable
+3.  declare port
+4.  set app.get('/)
+5.  lisen to port app.listen(port,()=>{
+6.  node index.js
+7.  check your browser for that port localhost:5000
+----------------------------
+**get
+1.cors middleware
+---------------------------------
+**post
+-----------------------------
+
+*/
